ffc4a56bdd56b48ecc1f6f49df3cdde2950b6a4c
suman khadka <sumankhadk@gmail.com>
2017-03-31 08:56:59 +0545

    first commit

---
+++ application/language/japanese/tasks_lang.php
@@ -0,0 +1,65 @@
+<?php
+$lang['complete_tasks'] = '完全なタスク';
+$lang['my_tasks'] = '私の仕事';
+$lang['save_tasks'] = 'タスクを保存';
+$lang['tasks'] = 'タスク';
+$lang['priority'] = '優先';
+$lang['task_add_success'] = 'タスクが正常に追加しました！';
+$lang['task_update_success'] = 'タスクが正常に更新します！';
+$lang['task_status'] = 'タスクの状態';
+$lang['task_name'] = 'タスク名';
+$lang['task_hours'] = 'タスク時間';
+$lang['task_assigned'] = 'タスクの割り当て';
+$lang['task_assigned_subject'] = '新しいタスクが割り当てられました！';
+$lang['task_file_uploaded'] = '新しいタスクのファイルがアップロード！';
+$lang['view_task'] = '[タスク';
+$lang['task_updated'] = 'タスクが正常に更新されました！';
+$lang['add_task'] = '新しいタスク';
+$lang['task'] = '仕事';
+$lang['assign_task'] = '新しい仕事';
+$lang['changes/view'] = '変更/ビュー';
+$lang['assined_to'] = 'に割り当てられた';
+$lang['estimated_hour'] = '推定時間';
+$lang['input_value'] = '全体の値を入力';
+$lang['task_description'] = 'タスクの説明';
+$lang['all_task'] = 'すべてのタスク';
+$lang['save_task'] = 'タスク情報が正常に追加されました！';
+$lang['activity_new_task'] = 'タスク情報が追加されました！';
+$lang['activity_update_task'] = 'タスク情報の更新！';
+$lang['activity_new_task_comment'] = 'タスク新しいコメント！';
+$lang['activity_new_leads_comment'] = '新しいコメントを導く！';
+$lang['activity_new_task_attachment'] = 'タスク新しい添付ファイル！';
+$lang['activity_new_leads_attachment'] = '新しい添付ファイルを導く！';
+$lang['update_task'] = 'タスク情報が正常に更新されました！';
+$lang['task_comment_save'] = 'タスクコメントが正常に更新されました！';
+$lang['leads_comment_save'] = 'コメントは成功裏に更新されました！';
+$lang['task_file_updated'] = 'タスクファイル情報が正常に更新されました';
+$lang['leads_file_updated'] = 'リードファイル情報が正常に更新されました';
+$lang['task_file_added'] = 'タスクファイル情報が正常に追加されました';
+$lang['leads_file_added'] = 'ファイル情報を正常にリードする';
+$lang['task_details'] = 'タスクの詳細';
+$lang['operation_failed'] = '操作に失敗しました ！';
+$lang['task_initials'] = 'タスクの頭文字';
+$lang['add_more'] = 'さらに追加';
+$lang['attach_file_list'] = '添付リスト';
+$lang['task_attachfile_deleted'] = '添付ファイルが正常に削除されました';
+$lang['leads_attachfile_deleted'] = '添付ファイルが正常に削除されました';
+$lang['activity_task_attachfile_deleted'] = '添付ファイルが削除されました';
+$lang['activity_task_deleted'] = '削除されたタスク情報';
+$lang['tasks_email'] = 'タスク電子メール';
+$lang['tasks_comments'] = 'タスクコメントが正常に更新されました！';
+$lang['tasks_attachment'] = 'タスクコメントが正常に更新されました！';
+$lang['tasks_updated'] = 'タスクコメントが正常に更新されました！';
+$lang['not_started'] = 'タスクコメントが正常に更新されました！';
+$lang['in_progress'] = 'タスクコメントが正常に更新されました！';
+$lang['completed'] = 'タスクコメントが正常に更新されました！';
+$lang['deferred'] = 'タスクコメントが正常に更新されました！';
+$lang['waiting_for_someone'] = 'タスクコメントが正常に更新されました！';
+$lang['participants'] = 'タスクコメントが正常に更新されました！';
+$lang['select_users'] = 'タスクコメントが正常に更新されました！';
+$lang['related_to'] = 'タスクコメントが正常に更新されました！';
+$lang['to_do'] = 'タスクコメントが正常に更新されました！';
+
+
+/* End of file tasks_lang.php */
+/* Location: ./application/language/japanese/tasks_lang.php */
